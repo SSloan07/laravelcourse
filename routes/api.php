5a3eec06292c2230603c183e1d65214a707a6277
SSloan07 <159721122+SSloan07@users.noreply.github.com>
2026-04-13 07:18:38 -0500

    Tutorial 4 completed

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Api Rest V1
+
+Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
+Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
+
+// Api Rest V2 
+
+Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
+Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');   
+
+// Api Rest V3 
+
+Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
+
+// Here we don't have a {id} for each product, which is initially different
